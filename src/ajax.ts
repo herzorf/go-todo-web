1256bde8982d773c415920e0fbbf60ed17ed8759
1446450047 <1446450047@qq.com>
2022-12-14 14:26:44 +0800

    将之前的项目内容迁移过来，并改为ts

---
+++ src/ajax.ts
@@ -0,0 +1,29 @@
+import axios from "axios";
+interface AjaxProps {
+    method: "post" | "get",
+    url: string
+    data: Object
+}
+function ajax({ method, url, data }: AjaxProps) {
+    const http = axios.create();
+
+    switch (method) {
+        case "get":
+            {
+                http.get(url, {
+                    data,
+                });
+            }
+            break;
+        case "post":
+            {
+                http.post(url, {
+                    data,
+                });
+            }
+            break;
+        default:
+            console.log("方法错误");
+    }
+}
+export default ajax;
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,38 +1,35 @@
-<script setup lang="ts">
-import { ref } from 'vue'
-
-defineProps<{ msg: string }>()
-
-const count = ref(0)
-</script>
-
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <el-form
+    ref="formRef"
+    :model="numberValidateForm"
+    label-width="100px"
+    :style="{ border: '1px solid red', display: 'flex' }"
+  >
+    <el-form-item label="todoName" prop="todoName">
+      <el-input v-model.number="numberValidateForm.todoName" type="text" />
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary" @click="submitForm()">Submit</el-button>
+    </el-form-item>
+  </el-form>
 </template>
+<script setup lang="ts">
+  import { reactive, ref } from "vue";
+  import ajax from "../ajax";
 
+  const formRef = ref();
+
+  const numberValidateForm = reactive({
+    todoName: "",
+  });
+
+  const submitForm = () => {
+    console.log(numberValidateForm.todoName);
+    ajax({ method: "post", url: "/api/v1/getTodo", data: numberValidateForm });
+  };
+</script>
 <style scoped>
-.read-the-docs {
-  color: #888;
-}
+  .read-the-docs {
+    color: #888;
+  }
 </style>
