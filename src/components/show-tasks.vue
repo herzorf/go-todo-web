f19c1f4a7de6cb223b5d7c1e106d9ed596e051e4
1446450047 <1446450047@qq.com>
2022-12-15 23:37:29 +0800

    添加task展示组件

---
+++ src/components/show-tasks.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>xxx</div>
+</template>
+<script setup lang="ts"></script>
+<style lang="css" setup></style>
--- src/home.vue
+++ src/home.vue
@@ -1,8 +1,11 @@
 <template>
+  <ShowTasks />
+  <br />
   <AddTask />
 </template>
 <script setup lang="ts">
   import AddTask from "./components/add-task.vue";
+  import ShowTasks from "./components/show-tasks.vue";
 </script>
 
 <style scoped></style>
